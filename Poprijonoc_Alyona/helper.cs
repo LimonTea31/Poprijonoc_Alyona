ebbc0d317b156327cdc54ed541787ab3ec5e053e
Alyona <LoverTea31@gmail.com>
2024-09-27 17:13:17 +0700

    Добавьте файлы проекта.

---
+++ Poprijonoc_Alyona/helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poprijonoc_Alyona
+{
+    public class helper
+    {
+        public static bool flag = false;
+        public static int prioritet = 0;
+
+        public static Entities1 ent;
+        public static Entities1 GetContext()
+        {
+            if (ent == null)
+            {
+                ent = new Entities1();
+            }
+            return ent;
+        }
+    }
+}
